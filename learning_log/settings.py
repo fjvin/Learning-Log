a8c7d9b62a54c0a0302fb91e08777687ed2ef024
fjvin <qfveatabay@tip.edu.ph>
2020-08-10 17:24:56 +0800

    Set DEBUG based on environment variables.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -137,3 +137,8 @@ BOOTSTRAP = {
 
 # Activate Django-Heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
